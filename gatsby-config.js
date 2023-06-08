27845579c405d8fa0597bef0f57fe67c579576c3
johnlt2001 <john.thornton2001@icloud.com>
2023-06-08 10:19:44 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `printthisforme`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
